8c653ccec92db334afc9704372b3d0ebb95c0b9d
Dave Faliskie <davefaliskie@gmail.com>
2017-09-20 22:11:35 -0400

    pull insta pics and display the 12 most recent

---
+++ app/assets/javascripts/instafeed_custom.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+
+  var userFeed = new Instafeed({
+    get: 'user',
+    userId: '38624178',
+    limit: 12,
+    resolution: 'standard_resolution',
+    accessToken: '38624178.1677ed0.e3ee4e06497644c5941743ec907e34bb',
+    sortBy: 'most-recent',
+    template: '<div class="col-lg-4 col-md-6 centered gallery instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+
+  });
+  userFeed.run();
+});
