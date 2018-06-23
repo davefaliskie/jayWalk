151f7cd0e6c36797a16d47fedc046cb7591d2f56
Dave Faliskie <davefaliskie@gmail.com>
2018-06-22 20:24:18 -0400

    update the IG access token

--- app/assets/javascripts/instafeed_custom.js
+++ app/assets/javascripts/instafeed_custom.js
@@ -5,7 +5,7 @@ $(document).on('turbolinks:load',function() {
     userId: '38624178',
     limit: 21,
     resolution: 'standard_resolution',
-    accessToken: '38624178.1677ed0.e3ee4e06497644c5941743ec907e34bb',
+    accessToken: '38624178.1677ed0.4488b30caa4f4ac882ccdc4f384e9083',
     sortBy: 'most-recent',
     template: '<div class="col-lg-4 centered gallery instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
 
